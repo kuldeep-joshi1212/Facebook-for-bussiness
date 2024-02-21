725064afebdace7c0e86e401932defbcd687f07f
kuldeep joshi <kuldeep.kj.joshi@gmail.com>
2024-02-22 05:21:23 +0530

    webhook v2

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import {NextResponse} from "next/server";
 
-export async function GET(req:Request) {
+export async function GET(req:Request,res:Response) {
     console.log(req)
     const{searchParams} =new URL(req.url);
     const mode=searchParams.get("hub.mode")
@@ -15,7 +15,9 @@ export async function GET(req:Request) {
         if (mode === "subscribe" && token === process.env.verifyToken) {
             // Respond with the challenge token from the request
             console.log("WEBHOOK_VERIFIED");
-            return NextResponse.json(challenge,{status:200});
+            // @ts-ignore
+            res.status(200).send(challenge);
+            // return NextResponse.json(challenge,{status:200});
         } else {
             // Respond with '403 Forbidden' if verify tokens do not match
             return NextResponse.json({status:403});
