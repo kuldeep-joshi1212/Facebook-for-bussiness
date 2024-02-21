d9896b810f113419947a66b39c7ea49227d8d0b8
kuldeep joshi <kuldeep.kj.joshi@gmail.com>
2024-02-22 05:11:30 +0530

    updated webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,16 +3,19 @@ import {NextResponse} from "next/server";
 
 export async function GET(req:Request) {
     console.log(req)
-    const{hub} =await req.json();
+    const{searchParams} =new URL(req.url);
+    const mode=searchParams.get("hub.mode")
+    const token=searchParams.get("hub.verify_token")
+    const challenge=searchParams.get("hub.challenge")
 
 
     // Check if a token and mode is in the query string of the request
-    if (hub.mode && hub.token) {
+    if (mode && token) {
         // Check the mode and token sent is correct
-        if (hub.mode === "subscribe" && hub.token === process.env.verifyToken) {
+        if (mode === "subscribe" && token === process.env.verifyToken) {
             // Respond with the challenge token from the request
             console.log("WEBHOOK_VERIFIED");
-            return NextResponse.json(hub.challenge,{status:200});
+            return NextResponse.json(challenge,{status:200});
         } else {
             // Respond with '403 Forbidden' if verify tokens do not match
             return NextResponse.json({status:403});
